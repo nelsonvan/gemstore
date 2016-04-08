3e2c49d7f1f6f2e4a5dc5c88f290f8b47f606528
nelson <nelsonvan321@gmail.com>
2016-04-08 03:12:10 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+
+
+(function(){
+	angular.module('store', []).controller('StoreController', function(){
+		this.products=gems;
+
+
+	});
+
+	
+
+})();
+var gems=[
+		{name:"Brilliant",
+		price:320.00,
+		description:'waw such much shining',
+		canPurchase:true,
+		soldOut:false
+		},
+		{name:"Brilliant",
+		price:320.00,
+		description:'waw such much shining',
+		canPurchase:true,
+		soldOut:false},
+		{name:"Brilliant",
+		price:320.00,
+		description:'waw such much shining',
+		canPurchase:true,
+		soldOut:false
+		},
+		{name:"Brilliant",
+		price:320.00,
+		description:'waw such much shining',
+		canPurchase:true,
+		soldOut:false}
+		];
